471cf08e19480d8efa7f7d6ba562c21aa8569a8e
HRDHRD5 <hrdhrd55@gmail.com>
2026-05-05 21:27:20 +0200

    .

--- measurements/measure.sh
+++ measurements/measure.sh
@@ -4,9 +4,9 @@ cd "$(dirname "$0")"
 
 RESULT_FILE="result.csv"
 TMP_FILE="tmp.csv"
+COVERT_CHANNELS=( "frb" "fra" )
 
 MEASUREMENTS=250
-COVERT_CHANNEL=frb
 
 echo "Saving CPU Specs"
 lscpu > cpu-specs.txt
@@ -16,20 +16,25 @@ lscpu --caches >> cpu-specs.txt
 echo "Building binary"
 make
 
-echo "Creating result File"
-echo "bit_count;stride;training_length;input;output;runtime" > $TMP_FILE
+for COVERT_CHANNEL in "${COVERT_CHANNELS[@]}"
+do
+    echo "Starting Measurements for Channel: ${COVERT_CHANNEL}"
+    echo ""
+    echo "Creating result File"
+    echo "bit_count;stride;training_length;input;output;runtime" > $TMP_FILE
 
-echo "Starting Measurements"
+    echo "Starting Measurements"
 
-for ((i = 0; i < MEASUREMENTS; i++)); do
-    echo "Run Nr ${i}/${MEASUREMENTS}"
-    for ((stride = 64; stride <= 4096; stride *= 2)) do
-        for ((bit_count = 1; bit_count <= 10; bit_count++)) do
-            for ((train_length = 0; train_length <= 150; train_length += 50)) do
-                ./measure "${COVERT_CHANNEL}" $RANDOM $bit_count $stride $train_length >> $TMP_FILE
+    for ((i = 0; i < MEASUREMENTS; i++)); do
+        echo "Run Nr ${i}/${MEASUREMENTS}"
+        for ((stride = 64; stride <= 4096; stride *= 2)) do
+            for ((bit_count = 1; bit_count <= 10; bit_count++)) do
+                for ((train_length = 0; train_length <= 150; train_length += 50)) do
+                    ./measure "${COVERT_CHANNEL}" $RANDOM $bit_count $stride $train_length >> $TMP_FILE
+                done
             done
         done
     done
-done
 
-cat $TMP_FILE | grep -v "Training:" > $RESULT_FILE
+    cat $TMP_FILE | grep -v "Training:" > "${COVERT_CHANNEL}-${RESULT_FILE}"
+done
