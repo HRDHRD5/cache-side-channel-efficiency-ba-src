8acd54adc5686f1c99a9615c8fcc7f3845cb624a
HRDHRD5 <hrdhrd55@gmail.com>
2026-05-13 22:27:10 +0200

    .

--- measurements/measure.sh
+++ measurements/measure.sh
@@ -25,11 +25,11 @@ do
 
     echo "Starting Measurements"
 
-    for ((i = 0; i < MEASUREMENTS; i++)); do
-        echo "Run Nr ${i}/${MEASUREMENTS}"
-        for ((stride = 64; stride <= 4096; stride *= 2)) do
-            for ((bit_count = 1; bit_count <= 10; bit_count++)) do
-                for ((train_length = 0; train_length <= 50; train_length += 10)) do
+    #echo "Run Nr ${i}/${MEASUREMENTS}"
+    for ((stride = 64; stride <= 4096; stride *= 2)) do
+        for ((bit_count = 1; bit_count <= 10; bit_count++)) do
+            for ((train_length = 0; train_length <= 50; train_length += 10)) do
+                for ((i = 0; i < MEASUREMENTS; i++)); do
                     ./measure "${COVERT_CHANNEL}" $i $bit_count $stride $train_length >> $TMP_FILE
                 done
             done
